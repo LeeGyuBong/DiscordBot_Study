8cb4df9bb8dccbd6aaa7f4f77549b488a5327abb
Bong <killo1729@gmail.com>
2024-09-23 16:13:55 +0900

    코드 정리

--- DiscordBot_Study/DiscordBot/DiscordBot.cs
+++ DiscordBot_Study/DiscordBot/DiscordBot.cs
@@ -3,49 +3,22 @@ using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using DiscordBot_Study;
 
 namespace DiscordBot
 {
     class Bot
     {
-        private DiscordSocketClient _client; // 봇 클라이언트
-
-        private static IConfiguration _configuration;
-        private static IServiceProvider _services;
-
-        private static readonly DiscordSocketConfig _discordSocketConfig = new()
-        {
-            LogLevel = LogSeverity.Verbose,
-            GatewayIntents = GatewayIntents.MessageContent
-        };
-
-        private static readonly InteractionServiceConfig _interactionServiceConfig = new()
-        {
-            LogLevel = LogSeverity.Verbose
-        };
+        private DiscordSocketClient? _client; // 봇 클라이언트
 
         public async Task BotMain()
         {
-            // Discord Config 로드
-            _configuration = new ConfigurationBuilder().
-                SetBasePath(AppContext.BaseDirectory).
-                AddJsonFile("Config.json").
-                Build();
-
-            _services = new ServiceCollection()
-                .AddSingleton(_configuration)
-                .AddSingleton(_discordSocketConfig)
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>(), _interactionServiceConfig))
-                .AddSingleton<InteractionHandler>()
-                .BuildServiceProvider();
-
-            _client = _services.GetRequiredService<DiscordSocketClient>();
+            _client = Program.Services.GetRequiredService<DiscordSocketClient>();
             _client.Log += LogAsync;
 
-            await _services.GetRequiredService<InteractionHandler>().InitializeAsync();
+            await Program.Services.GetRequiredService<InteractionHandler>().InitializeAsync();
 
-            await _client.LoginAsync(TokenType.Bot, _configuration["Token"]);
+            await _client.LoginAsync(TokenType.Bot, Program.Configuration["DiscordToken"]);
             await _client.StartAsync();
 
             await Task.Delay(Timeout.Infinite);
--- DiscordBot_Study/Program.cs
+++ DiscordBot_Study/Program.cs
@@ -1,11 +1,40 @@
-﻿using DiscordBot;
+﻿using Discord.Interactions;
+using Discord;
+using Discord.WebSocket;
+using DiscordBot;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscordBot_Study
 {
     public class Program
     {
+        // Discord Config 로드
+        public static readonly IConfiguration Configuration = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("Config.json").Build();
+
+        public static IServiceProvider? Services;
+
+        private static readonly DiscordSocketConfig _discordSocketConfig = new()
+        {
+            LogLevel = LogSeverity.Verbose,
+            GatewayIntents = GatewayIntents.MessageContent
+        };
+
+        private static readonly InteractionServiceConfig _interactionServiceConfig = new()
+        {
+            LogLevel = LogSeverity.Verbose
+        };
+
         public static void Main(string[] args)
-        { 
+        {
+            Services = new ServiceCollection()
+                .AddSingleton(Configuration)
+                .AddSingleton(_discordSocketConfig)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>(), _interactionServiceConfig))
+                .AddSingleton<InteractionHandler>()
+                .BuildServiceProvider();
+
             new Bot().BotMain().GetAwaiter().GetResult();
         }
     }
